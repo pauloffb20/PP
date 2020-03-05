7de7f505e3eca132fa8ea9c83f8f8a666bf46275
8160279 <8160279@estg.ipp.pt>
2020-03-05 10:11:48 +0000

    first commit

---
+++ Ex1F1/src/main/java/Ex1F1/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ex1F1;
+
+/**
+ *
+ * @author Paulo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+ public static void main(String[] args) {
+        // TODO code application logic here
+        char l = 'l', p = 'p';
+        int q = 4, d = 2;
+        System.out.println(l);
+        System.out.println(p);
+        System.out.println(d);
+        System.out.println(q);
+        System.out.println(l + p + 2);
+        System.out.println(""+ l + p +2);
+        System.out.println(q + d);
+    }
+    
+}
+    
+
